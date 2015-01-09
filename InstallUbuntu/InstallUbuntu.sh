b9ba7b34fa73a35622cc8d2d25c5030ae8278263
sls <sls@houxiaomu-VirtualBox.(none)>
2015-01-09 09:39:19 +0800

    install software

--- InstallUbuntu/InstallUbuntu.sh
+++ InstallUbuntu/InstallUbuntu.sh
@@ -38,25 +38,8 @@
 echo -e "\033[42;37m update apt-get \033[0m"
 #update:apt-get server database;upgrade:software version 
 sudo apt-get update;
-echo -e "\033[41;33m install rar \033[0m"
-#解压缩用 rar x <file> .
-sudo apt-get install rar;
 
-echo -e "\033[41;33m install vim \033[0m"
-sudo apt-get install vim;
-
-#文件对比
-echo -e "\033[41;33m install meld \033[0m"
-sudo apt-get install meld;
-
-#用于制作usb系统安装盘
-echo -e "\033[41;33m install unetbooin \033[0m"
-sudo apt-get install unetbootin;
-
-#player
-echo -e "\033[41;33m install smplayer\033[0m"
-sudo apt-get install smplayer; 
-sudo apt-get remove vlc;
+./installSoftware.sh
 
 #browser
 echo -e "\033[41;33m install chrome\033[0m"
---
+++ InstallUbuntu/installSoftware.sh
@@ -0,0 +1,20 @@
+o -e "\033[41;33m install rar \033[0m"
+#解压缩用 rar x <file> .
+sudo apt-get install rar;
+
+echo -e "\033[41;33m install vim \033[0m"
+sudo apt-get install vim;
+
+#文件对比
+echo -e "\033[41;33m install meld \033[0m"
+sudo apt-get install meld;
+
+#用于制作usb系统安装盘
+echo -e "\033[41;33m install unetbooin \033[0m"
+sudo apt-get install unetbootin;
+
+#player
+echo -e "\033[41;33m install smplayer\033[0m"
+sudo apt-get install smplayer; 
+sudo apt-get remove vlc;
+
