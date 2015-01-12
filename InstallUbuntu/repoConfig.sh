0d01a76d6af28c4633d14fb99b356b696e830810
sls <sls@houxiaomu-VirtualBox.(none)>
2015-01-12 17:24:51 +0800

    curl

--- InstallUbuntu/repoConfig.sh
+++ InstallUbuntu/repoConfig.sh
@@ -1,5 +1,5 @@
 echo -e "\n############# repo config #############\n"
-
+sudo apt-get install curl;
 sudo curl https://storage.googleapis.com/git-repo-downloads/repo > repo
 sudo mkdir /opt/sls
 sudo chmod 777 repo
