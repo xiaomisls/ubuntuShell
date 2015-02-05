21aab4b9efada7c998935b0e68d5071c7109b04c
sunlishan <sunlishan@xiaomi.com>
2015-02-05 17:06:25 +0800

    c

---
+++ c/a.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(int p,char **argv){
+    printf("----------------------\n");
+    /*char *a = "abc";
+    printf("%s",a);
+    */
+    printf("%d\n",p);
+    printf("\n----------------------\n");
+}
