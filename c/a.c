64f8aa2b8c964f737704127ee573a0b2f7967e5f
sunlishan <sunlishan@xiaomi.com>
2015-02-05 18:17:00 +0800

    c:getopt

--- c/a.c
+++ c/a.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 int main(int p,char **argv){
     printf("----------------------\n");
     /*char *a = "abc";
     printf("%s",a);
     */
-    printf("%d\n",p);
+    char ch='a';
+    while((ch = getopt(p,argv,"a:b")) !=-1){
+        printf("%c,%s\n",ch,optarg);
+    }
+
     printf("\n----------------------\n");
 }
