2e41831083771e03a4ab4e851aa58f25d9d302cf
sunlishan <sunlishan@xiaomi.com>
2015-03-19 15:08:22 +0800

    shell script

--- load_my_script.sh
+++ load_my_script.sh
@@ -1,5 +1,5 @@
-echo "\nload my shell scripts in $SAMARIUM_SCRIPT_PATH\n";
-for i in `find $SAMARIUM_PATH -name "sls_*_helper.sh"`
+echo "-----load my shell scripts in $SAMARIUM_SCRIPT_PATH -----";
+for i in `find $SAMARIUM_SCRIPT_PATH -name "sls_*_helper.sh"`
 do 
     source $i;
 done
